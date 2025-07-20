1acbad67f81dc573db7f10cf7f0b73c2379aa443
halimov <halimov@infomatika.ru>
2025-07-20 20:21:36 +0300

    add cart counter, cart actions (add, remove)

--- components/AddToCartButton/AddToCartButton.tsx
+++ components/AddToCartButton/AddToCartButton.tsx
@@ -1,6 +1,9 @@
 import { useTranslations } from 'next-intl';
 import { FaCartShopping } from 'react-icons/fa6';
 import './AddToCartButton.scss';
+import { useCartStore } from '@/store/cartStore';
+import { Link } from '@/i18n/navigation';
+import { useEffect } from 'react';
 
 export default function AddToCartButton({
     productId,
@@ -9,8 +12,34 @@ export default function AddToCartButton({
 }) {
     const t = useTranslations('common');
 
+    const cart = useCartStore(state => state.cart);
+    const addToCart = useCartStore(state => state.addToCart);
+    const removeFromCart = useCartStore(state => state.removeFromCart);
+    const itemInCartAmount = cart.find(item => item.id === productId && item.quantity > 0)?.quantity || 0;
+
+    function add() {
+        addToCart(productId);
+    }
+
+    function remove() {
+        removeFromCart(productId);
+    }
+
+    if(itemInCartAmount > 0) {
+        return (
+            <span className="flex items-center gap-[5px]">
+                <Link href="/cart">{t("In cart")}</Link>
+                <span className="flex items-center">
+                    <button onClick={add}>+</button>
+                    {itemInCartAmount}
+                    <button onClick={remove}>-</button>
+                </span>
+            </span>
+        )
+    }
+
     return (
-        <button className="add-to-cart-button">
+        <button onClick={add} className="add-to-cart-button">
             <FaCartShopping className="add-to-cart-button__icon" />
             <span className="add-cart-button__text">{t('Add to Cart')}</span>
         </button>
--- store/cartStore.ts
+++ store/cartStore.ts
@@ -3,26 +3,49 @@ import type { Product } from '@/types';
 import { products } from '@/data/products';
 
 interface CartStore {
-    cart: (number | string)[];
-    addToCart: (id: number) => void;
-    getCart: () => Product[];
+    cart: {id: number | string; quantity: number}[];
+    addToCart: (id: number | string) => void;
+    removeFromCart: (id: number | string) => void;
     getCartCount: () => number;
 }
 
 export const useCartStore = create<CartStore>((set, get) => ({
-    cart: <(number | string)[]>[],
+    cart: <{id: number | string; quantity: number}[]>([]),
     addToCart: (id: number | string) => {
-        set((state) => ({
-            cart: [...state.cart, id],
-        }));
+
+        const cart = get().cart;
+        const findItemInCart = cart.find(item => item.id === id);
+
+        if(findItemInCart) {
+            set((state) => {
+                return {
+                    cart: [{id, quantity: findItemInCart.quantity + 1}, ...state.cart.filter(item => item.id !== id)]
+                }
+            })
+        } else {
+            set((state) => ({
+                cart: [{id, quantity: 1}, ...state.cart]
+            }))
+        }
     },
-    getCart: () => {
-        const { cart } = get();
-        return products.filter((product) => cart.includes(product.id));
+
+    removeFromCart: (id: number | string) => {
+        set((state) => {
+            const matchItem = state.cart.find(item => item.id === id);
+
+            if(matchItem && matchItem.quantity > 1) {
+                return {
+                    cart: [{id, quantity: matchItem.quantity - 1}, ...state.cart.filter(item => item.id !== id)]
+                }
+            }
+            
+            return {
+                cart: [...state.cart.filter(item => item.id !== id)]
+            }
+        })
     },
+    
     getCartCount: () => {
-        const { getCart } = get();
-        const cart = getCart();
-        return cart.length;
+        return get().cart.reduce((acc, item) => acc + item.quantity, 0);
     },
 }));
