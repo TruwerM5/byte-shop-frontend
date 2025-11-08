7036d6c536218f74323afb377d9639d5cb8ead00
halimov <halimov@infomatika.ru>
2025-11-08 18:34:07 +0300

    edit filters.tsx, init filters.scss

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,47 +1,51 @@
 'use client';
 import { useTranslations } from 'next-intl';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { beutifyFilterInputPrice } from '@/utils/beautifyPrice';
+import Accordion from '../Accordion/Accordion';
+import './Filters.scss';
 
 export default function Filters({
+    productSlug,
     applyFilters
 }: {
+    productSlug: string;
     applyFilters: (query: string, value: string) => void;
 }) {
     const t = useTranslations('common');
 
-    function handleInputChange(query: string, value: string) {
+    const handleInputChange = useDebouncedCallback((query: string, value: string) => {
         applyFilters(query, value);
-    }
+    }, 300);
 
     return (
         <div className='filters'>
             <span className='filters__title'>
                 {t('Filters')}
             </span>
-            <div className='flex gap-[10px]'>
-                <FilterPriceInput 
-                    type='from' 
-                    initialValue={''} 
-                    setQueryCallback={handleInputChange}
-                />
-                <FilterPriceInput 
-                    type='to' 
-                    initialValue={''} 
-                    setQueryCallback={handleInputChange}
-                />
-            </div>
+            <Accordion title='Price'>
+                <div className='flex gap-[10px]'>
+                    <FilterPriceInput
+                        type='from'
+                        setQueryCallback={handleInputChange}
+                    />
+                    <FilterPriceInput
+                        type='to'
+                        setQueryCallback={handleInputChange}
+                    />
+                </div>
+            </Accordion>
         </div>
     )
 }
 
 function FilterPriceInput({
     type,
-    initialValue,
     setQueryCallback
 }: {
     type: 'from' | 'to';
-    initialValue: string;
     setQueryCallback: (query: string, value: string) => void;
 }) {
 
@@ -55,13 +59,18 @@ function FilterPriceInput({
     const [price, setPrice] = useState<string>(currentQuery);
     
     function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const value = e.target.value;
-        if(isNaN(Number(value))) {
-            e.target.value = '';
+        const value = removeNonNumbers(e.target.value);
+        e.target.value = value;
+        if(value.length === 0) {
+            setQuery(query, value);
             return;
         }
+        if(Number(value) === 0) {
+            e.target.value = '';
+        } 
         setPrice(value);
         setQuery(query, value);
+        e.target.value = beutifyFilterInputPrice(value);
     }
 
     function setQuery(query: string, value: string) {
@@ -74,13 +83,17 @@ function FilterPriceInput({
         setQueryCallback(query, value);
     }
 
+    function removeNonNumbers(value: string) {
+        return value.split('').filter(c => !isNaN(Number(c)) && c !== ' ').join('');
+    }
+
     return (
         <input
             onChange={onInputChange}
-            className='filters__price-input max-w-1/2 border-1 border-b border-black rounded-[3px] px-[5px] py-[3px] outline-none' 
+            className='filters__price-input' 
             type='text' 
             placeholder={placeholder}
-            defaultValue={price}
+            defaultValue={beutifyFilterInputPrice(price)}
         />
     )
 
