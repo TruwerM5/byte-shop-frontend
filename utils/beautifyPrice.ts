64dcd5079926a7d5a06d6facbbdfddb857720b17
halimov <halimov@infomatika.ru>
2025-11-08 18:31:44 +0300

    add beautify function for filter input

--- utils/beautifyPrice.ts
+++ utils/beautifyPrice.ts
@@ -1,8 +1,19 @@
-export default function beautifyPrice(price: number) {
+export function beautifyProductPrice(price: number) {
     let rest = `${price % 1000}`;
 
     while (rest.length < 3) {
         rest += '0';
     }
     return `${(Math.floor(price / 1000))} ${rest}`;
+}
+
+export function beutifyFilterInputPrice(price: string) {
+    if(price.length <= 3) {
+        return price.split('').filter(l => !isNaN(Number(l))).join('');
+    }
+    let rest = `${Number(price) % 1000}`;
+    while (rest.length < 3) {
+        rest += '0';
+    }
+    return `${(Math.floor(Number(price) / 1000))} ${rest}`;
 }
