c7360bad8b1ae08fad8691ca751b308373d5f01b
halimov <halimov@infomatika.ru>
2025-11-08 18:33:16 +0300

    remove promises from [category] page

--- app/[locale]/(public)/catalog/[category]/page.tsx
+++ app/[locale]/(public)/catalog/[category]/page.tsx
@@ -12,15 +12,10 @@ export default async function Page({
 }) {
     
     const { category } = await params;
-    const [tProducts, tCommon] = await Promise.all([
-        getTranslations('slugs'),
-        getTranslations('common'),
-    ]);
 
     return (
         <div className='category-page'>
             <Search />
-            <h3 className='category-page__title'>{tProducts(category)}</h3>
             <div className='category-page__inner'>                
                 <div className='category-page__product-list-wrapper'>
                     <ProductList category={category} />
