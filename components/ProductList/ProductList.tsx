c0f9d13a78bb5dab5bbaaaf2fb96cbe334fb1367
halimov <halimov@infomatika.ru>
2025-11-24 22:49:57 +0300

    edit ProductList

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -10,6 +10,8 @@ import { useProductStore } from '@/store/productStore';
 import { fetchProductsByParamOrSlug } from '@/api/products';
 import CatalogSkeleton from '@/components/UI/Skeletons/CatalogSkeleton/CatalogSkeleton';
 import { useTranslations } from 'next-intl';
+import { useSearchParams } from 'next/navigation';
+
 type ProductsState = 'pending' | 'success' | 'error' | 'not-found';
 
 export default function ProductList({ 
@@ -17,21 +19,30 @@ export default function ProductList({
 }: { 
     category: string;
 }) {
-    
+    const searchParams = useSearchParams();
     const { storeProducts, getProductsByCategory } = useProductStore();
     const [products, setProducts] = useState<Product[]>([]);
     const [productsState, setProductsState] = useState<ProductsState>('pending');
-    const [filters, setFilters] = useState({});
+    const [filters, setFilters] = useState({
+        price_min: searchParams.get('price_min'),
+        price_max: searchParams.get('price_max'),
+        socket: searchParams.get('socket')?.toString().split(',') || [],
+        line: searchParams.get('line')?.toString().split(',') || [],
+        'cpu-manufacturer': searchParams.get('cpu-manufacturer')?.toString().split(',') || [],
+    });
     const [message, setMessage] = useState('');
     const tCommon = useTranslations('common');
+    const tSlugs = useTranslations('slugs');
 
     function handleSort(sorted: Product[]) {
         setProducts(sorted);
     }
 
-    function applyFilters(query: string, value: string) {
-        const newFilters = {...filters, [query]: value};
-        setFilters(newFilters);
+    function applyFilters(query: string, value: string[]){
+        setFilters(prev => ({
+            ...prev,
+            [query]: value,
+        }));
     }
 
     function getProducts(category: string, filters?: any) {
@@ -72,27 +83,30 @@ export default function ProductList({
     } 
 
     return (
-        <div className='products-wrapper'>
-            <div className='filters-wrapper'>
-                <Filters applyFilters={applyFilters} />
+        <>
+            <h3 className='category-page__title'>{tSlugs(category)}</h3>
+            <div className='products-wrapper'>
+                <div className='filters-wrapper'>
+                    <Filters productSlug={category} applyFilters={applyFilters} />
+                </div>
+                <div className='product-list-wrapper'>
+                    {products.length > 0 ? (
+                        <>
+                            <div className='applied-filters'>
+                                Applied filters
+                            </div>
+                            <SortCategories products={products} onSort={handleSort} />
+                            <div className='product-list'>
+                                {products.map((product) => (
+                                    <ProductItem product={product} key={product.id} />
+                                ))}
+                            </div>
+                        </>
+                    ) : (
+                        <p className='not-found-message'>{tCommon('Nothing found matching your request')}</p>
+                    )}
+                </div>
             </div>
-            <div className='product-list-wrapper'>
-                {products.length > 0 ? (
-                    <>
-                        <div className='applied-filters'>
-                            Applied filters
-                        </div>
-                        <SortCategories products={products} onSort={handleSort} />
-                        <div className='product-list'>
-                            {products.map((product) => (
-                                <ProductItem product={product} key={product.id} />
-                            ))}
-                        </div>
-                    </>
-                ) : (
-                    <p className='not-found-message'>{tCommon('Nothing found matching your request')}</p>
-                )}
-            </div>
-        </div>      
+        </>
     );
 }
